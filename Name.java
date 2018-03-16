c20dde37b69f90571e7cf2d2207ed83771fdcab4
LAPTOP-LFOI3GB6\Korosuke <ddt.koroki@gmail.com>
2018-03-16 13:51:07 +0900

    新明解Java入門編の演習1-2～2-2までを提出します。

---
+++ Name.java
@@ -0,0 +1,10 @@
+package chap01;
+
+public class Name {
+
+	public static void main(String[] args) {
+		System.out.print("興\n梠\n祐\n輔");
+
+	}
+
+}
---
+++ Name2.java
@@ -0,0 +1,8 @@
+package chap01;
+
+public class Name2 {
+
+	public static void main(String[] args) {
+		System.out.print("興\n梠\n\n祐\n輔");
+	}
+}
