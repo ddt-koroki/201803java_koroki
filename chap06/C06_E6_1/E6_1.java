5bea5bc95f6add31281350afeb68921feed33768
LAPTOP-LFOI3GB6\Korosuke <ddt.koroki@gmail.com>
2018-04-26 16:50:26 +0900

    演習6-1を提出します。

---
+++ chap06/C06_E6_1/E6_1.java
@@ -0,0 +1,44 @@
+package C06.E6_1;
+
+//演習「6-1」要素型がdouble型で要素数5の配列の全要素を表示するプログラム
+
+public class E6_1 {
+
+	public static void main(String[] args) {
+
+		//要素型がdouble型で要素数が5の変数を作成
+		double[] realNo = new double[5];
+
+		//要素0に値を導入
+		realNo[0] = 4.49;
+
+		//要素1に値を導入
+		realNo[1] = 9.94;
+
+		//要素2に値を導入
+		realNo[2] = 87.9;
+
+		//要素3に値を導入
+		realNo[3] = 9.44;
+
+		//要素4に値を導入
+		realNo[4] = 0.98;
+
+		//要素0の値を画面に表示
+		System.out.println("入力された実数は" + realNo[0] + "です。");
+
+		//要素1の値を画面に表示
+		System.out.println("入力された実数は" + realNo[1] + "です。");
+
+		//要素2の値を画面に表示
+		System.out.println("入力された実数は" + realNo[2] + "です。");
+
+		//要素3の値を画面に表示
+		System.out.println("入力された実数は" + realNo[3] + "です。");
+
+		//要素4の値を画面に表示
+		System.out.println("入力された実数は" + realNo[4] + "です。");
+
+	}
+
+}
